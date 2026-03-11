0dcb9cb22a80e9d3095681b12671bad37c5fe051
Jon Love <124639155+jonnny013@users.noreply.github.com>
2026-03-11 20:01:55 +0800

    Attempt to make countdown

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,29 @@
 #include <Arduino.h>
 
 static int buttonPin = 2;
-static int ledPin = 13;
+static int redLed1Pin = 5;
+static int redLed2Pin = 4;
+static int greenLedPin = 3;
 static int buttonState = 0;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Hello world");
-  pinMode(ledPin, OUTPUT);
+  pinMode(redLed1Pin, OUTPUT);
+  pinMode(redLed2Pin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
   pinMode(buttonPin, INPUT);
 };
 
 void loop() {
   buttonState = digitalRead(buttonPin);
   if (buttonState == 1) {
-    digitalWrite(ledPin, HIGH);
-
-    Serial.println("pushed ---");
+    digitalWrite(redLed1Pin, HIGH);
+    delay(1000);
+    digitalWrite(redLed2Pin, HIGH);
+    delay(1000);
+    digitalWrite(greenLedPin, HIGH);
   } else {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(redLed1Pin, LOW);
+    digitalWrite(redLed2Pin, LOW);
+    digitalWrite(greenLedPin, LOW);
   };
 };
